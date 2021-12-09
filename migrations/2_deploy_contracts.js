f566a846b8f246e651ce954dc899f97a59b4ba89
Karoliskved <79286725+Karoliskved@users.noreply.github.com>
2021-12-09 22:08:52 +0200

    Add files via upload

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var Sutartis= artifacts.require("transaction");
 
 module.exports = function(deployer){
-    deployer.deploy(Sutartis, "0x1d1d36Aa5d2569662985F06bD20993DB46FAa24F");
+    deployer.deploy(Sutartis, "0x19b902347590465e2D5E52D560d407f31C8BF256");
 }
